ac783ce6ebdd199817396ec5cb05e52a9ad71677
HEX9CF <hex9cf@aliyun.com>
2024-10-22 12:20:22 +0800

    Update test_data.sql

--- test_data.sql
+++ test_data.sql
@@ -4,16 +4,16 @@ USE stuoj_db;
 
 -- Insert test data into tbl_judgement
 INSERT INTO tbl_judgement (submission_id, status, time, memory) VALUES
-                                                                    (1, 1, 100, 256),
-                                                                    (2, 0, 200, 512),
-                                                                    (3, 1, 150, 128),
-                                                                    (4, 0, 300, 1024),
-                                                                    (5, 1, 250, 2048),
-                                                                    (6, 0, 350, 4096),
-                                                                    (7, 1, 400, 8192),
-                                                                    (8, 0, 450, 16384),
-                                                                    (9, 1, 500, 32768),
-                                                                    (10, 0, 550, 65536);
+                                                                    (1, 0, 100, 256),
+                                                                    (2, 1, 200, 512),
+                                                                    (3, 0, 150, 128),
+                                                                    (4, 1, 300, 1024),
+                                                                    (5, 0, 250, 256),
+                                                                    (6, 1, 350, 512),
+                                                                    (7, 0, 400, 128),
+                                                                    (8, 1, 450, 1024),
+                                                                    (9, 0, 500, 256),
+                                                                    (10, 1, 550, 512);
 
 -- Insert test data into tbl_language
 INSERT INTO tbl_language (name) VALUES
@@ -23,49 +23,49 @@ INSERT INTO tbl_language (name) VALUES
                                     ('JavaScript'),
                                     ('Ruby'),
                                     ('Go'),
+                                    ('Rust'),
                                     ('Swift'),
                                     ('Kotlin'),
-                                    ('PHP'),
-                                    ('Rust');
+                                    ('PHP');
 
 -- Insert test data into tbl_problem
-INSERT INTO tbl_problem (title, source, difficulty, time_limit, memory_limit, description, input, output, sample_input, sample_output, hint, create_time, update_time) VALUES
-                                                                                                                                                                           ('两数之和', 'LeetCode', 2, 1, 128, '描述1', '输入1', '输出1', '样例输入1', '样例输出1', '提示1', '2023-01-01 10:00:00', '2023-01-01 10:00:00'),
-                                                                                                                                                                           ('反转字符串', 'LeetCode', 1, 1, 128, '描述2', '输入2', '输出2', '样例输入2', '样例输出2', '提示2', '2023-01-02 11:00:00', '2023-01-02 11:00:00'),
-                                                                                                                                                                           ('回文数', 'LeetCode', 1, 1, 128, '描述3', '输入3', '输出3', '样例输入3', '样例输出3', '提示3', '2023-01-03 12:00:00', '2023-01-03 12:00:00'),
-                                                                                                                                                                           ('合并两个有序链表', 'LeetCode', 2, 1, 128, '描述4', '输入4', '输出4', '样例输入4', '样例输出4', '提示4', '2023-01-04 13:00:00', '2023-01-04 13:00:00'),
-                                                                                                                                                                           ('有效的括号', 'LeetCode', 2, 1, 128, '描述5', '输入5', '输出5', '样例输入5', '样例输出5', '提示5', '2023-01-05 14:00:00', '2023-01-05 14:00:00'),
-                                                                                                                                                                           ('最大子序和', 'LeetCode', 3, 1, 128, '描述6', '输入6', '输出6', '样例输入6', '样例输出6', '提示6', '2023-01-06 15:00:00', '2023-01-06 15:00:00'),
-                                                                                                                                                                           ('爬楼梯', 'LeetCode', 1, 1, 128, '描述7', '输入7', '输出7', '样例输入7', '样例输出7', '提示7', '2023-01-07 16:00:00', '2023-01-07 16:00:00'),
-                                                                                                                                                                           ('买卖股票的最佳时机', 'LeetCode', 2, 1, 128, '描述8', '输入8', '输出8', '样例输入8', '样例输出8', '提示8', '2023-01-08 17:00:00', '2023-01-08 17:00:00'),
-                                                                                                                                                                           ('对称二叉树', 'LeetCode', 2, 1, 128, '描述9', '输入9', '输出9', '样例输入9', '样例输出9', '提示9', '2023-01-09 18:00:00', '2023-01-09 18:00:00'),
-                                                                                                                                                                           ('二叉树的中序遍历', 'LeetCode', 2, 1, 128, '描述10', '输入10', '输出10', '样例输入10', '样例输出10', '提示10', '2023-01-10 19:00:00', '2023-01-10 19:00:00');
+INSERT INTO tbl_problem (title, source, difficulty, time_limit, memory_limit, description, input, output, sample_input, sample_output, hint) VALUES
+                                                                                                                                                 ('Problem 1', 'Source 1', 1, 1, 128, 'Description 1', 'Input 1', 'Output 1', 'Sample Input 1', 'Sample Output 1', 'Hint 1'),
+                                                                                                                                                 ('Problem 2', 'Source 2', 2, 2, 256, 'Description 2', 'Input 2', 'Output 2', 'Sample Input 2', 'Sample Output 2', 'Hint 2'),
+                                                                                                                                                 ('Problem 3', 'Source 3', 3, 3, 512, 'Description 3', 'Input 3', 'Output 3', 'Sample Input 3', 'Sample Output 3', 'Hint 3'),
+                                                                                                                                                 ('Problem 4', 'Source 4', 4, 4, 1024, 'Description 4', 'Input 4', 'Output 4', 'Sample Input 4', 'Sample Output 4', 'Hint 4'),
+                                                                                                                                                 ('Problem 5', 'Source 5', 5, 5, 128, 'Description 5', 'Input 5', 'Output 5', 'Sample Input 5', 'Sample Output 5', 'Hint 5'),
+                                                                                                                                                 ('Problem 6', 'Source 6', 6, 6, 256, 'Description 6', 'Input 6', 'Output 6', 'Sample Input 6', 'Sample Output 6', 'Hint 6'),
+                                                                                                                                                 ('Problem 7', 'Source 7', 1, 7, 512, 'Description 7', 'Input 7', 'Output 7', 'Sample Input 7', 'Sample Output 7', 'Hint 7'),
+                                                                                                                                                 ('Problem 8', 'Source 8', 2, 8, 1024, 'Description 8', 'Input 8', 'Output 8', 'Sample Input 8', 'Sample Output 8', 'Hint 8'),
+                                                                                                                                                 ('Problem 9', 'Source 9', 3, 9, 128, 'Description 9', 'Input 9', 'Output 9', 'Sample Input 9', 'Sample Output 9', 'Hint 9'),
+                                                                                                                                                 ('Problem 10', 'Source 10', 4, 10, 256, 'Description 10', 'Input 10', 'Output 10', 'Sample Input 10', 'Sample Output 10', 'Hint 10');
 
 -- Insert test data into tbl_submission
-INSERT INTO tbl_submission (user_id, problem_id, status, score, submit_time, language_id, length, memory, time, solution) VALUES
-                                                                                                                              (1, 1, 1, 100, '2023-01-01 10:00:00', 1, 50, 256, 100, '题解1'),
-                                                                                                                              (2, 2, 0, 90, '2023-01-02 11:00:00', 2, 60, 512, 200, '题解2'),
-                                                                                                                              (3, 3, 1, 80, '2023-01-03 12:00:00', 3, 70, 128, 150, '题解3'),
-                                                                                                                              (4, 4, 0, 70, '2023-01-04 13:00:00', 4, 80, 1024, 300, '题解4'),
-                                                                                                                              (5, 5, 1, 60, '2023-01-05 14:00:00', 5, 90, 2048, 250, '题解5'),
-                                                                                                                              (6, 6, 0, 50, '2023-01-06 15:00:00', 6, 100, 4096, 350, '题解6'),
-                                                                                                                              (7, 7, 1, 40, '2023-01-07 16:00:00', 7, 110, 8192, 400, '题解7'),
-                                                                                                                              (8, 8, 0, 30, '2023-01-08 17:00:00', 8, 120, 16384, 450, '题解8'),
-                                                                                                                              (9, 9, 1, 20, '2023-01-09 18:00:00', 9, 130, 32768, 500, '题解9'),
-                                                                                                                              (10, 10, 0, 10, '2023-01-10 19:00:00', 10, 140, 65536, 550, '题解10');
+INSERT INTO tbl_submission (user_id, problem_id, status, score, language_id, length, memory, time, solution) VALUES
+                                                                                                                 (1, 1, 0, 100, 1, 1000, 256, 100, 'Solution 1'),
+                                                                                                                 (2, 2, 1, 90, 2, 2000, 512, 200, 'Solution 2'),
+                                                                                                                 (3, 3, 0, 80, 3, 1500, 128, 150, 'Solution 3'),
+                                                                                                                 (4, 4, 1, 70, 4, 3000, 1024, 300, 'Solution 4'),
+                                                                                                                 (5, 5, 0, 60, 5, 2500, 256, 250, 'Solution 5'),
+                                                                                                                 (6, 6, 1, 50, 6, 3500, 512, 350, 'Solution 6'),
+                                                                                                                 (7, 7, 0, 40, 7, 4000, 128, 400, 'Solution 7'),
+                                                                                                                 (8, 8, 1, 30, 8, 4500, 1024, 450, 'Solution 8'),
+                                                                                                                 (9, 9, 0, 20, 9, 5000, 256, 500, 'Solution 9'),
+                                                                                                                 (10, 10, 1, 10, 10, 5500, 512, 550, 'Solution 10');
 
 -- Insert test data into tbl_test_point
-INSERT INTO tbl_test_point (problem_id, test_input, test_output) VALUES
-                                                                     (1, '测试输入1', '测试输出1'),
-                                                                     (2, '测试输入2', '测试输出2'),
-                                                                     (3, '测试输入3', '测试输出3'),
-                                                                     (4, '测试输入4', '测试输出4'),
-                                                                     (5, '测试输入5', '测试输出5'),
-                                                                     (6, '测试输入6', '测试输出6'),
-                                                                     (7, '测试输���7', '测试输出7'),
-                                                                     (8, '测试输入8', '测试输出8'),
-                                                                     (9, '测试输入9', '测试输出9'),
-                                                                     (10, '测试输入10', '测试输出10');
+INSERT INTO tbl_test_point (serial, problem_id, test_input, test_output) VALUES
+                                                                                    (1, 1, 'Test Input 1', 'Test Output 1'),
+                                                                                    (2, 2, 'Test Input 2', 'Test Output 2'),
+                                                                                    (3, 3, 'Test Input 3', 'Test Output 3'),
+                                                                                    (4, 4, 'Test Input 4', 'Test Output 4'),
+                                                                                    (5, 5, 'Test Input 5', 'Test Output 5'),
+                                                                                    (6, 6, 'Test Input 6', 'Test Output 6'),
+                                                                                    (7, 7, 'Test Input 7', 'Test Output 7'),
+                                                                                    (8, 8, 'Test Input 8', 'Test Output 8'),
+                                                                                    (9, 9, 'Test Input 9', 'Test Output 9'),
+                                                                                    (10, 10, 'Test Input 10', 'Test Output 10');
 
 -- Insert test data into tbl_user
 INSERT INTO tbl_user (username, password, role, email, avatar, create_time, update_time) VALUES
