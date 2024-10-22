35ac85430b0abcce880a09cecaa05e77884d3c53
HEX9CF <hex9cf@aliyun.com>
2024-10-22 12:06:18 +0800

    Change some data type text to longtext

--- create_db.sql
+++ create_db.sql
@@ -32,11 +32,12 @@ create table if not exists tbl_problem
     difficulty    int unsigned default '0'     not null comment '难度： 0 暂无评定，1 普及−，2 普及/提高−，3 普及+/提高，4 提高+/省选− ，5 省选/NOI−，6 NOI/NOI+/CTSC',
     time_limit    int unsigned default '1'     not null comment '时间限制（秒）',
     memory_limit  int unsigned default '128'   not null comment '内存限制（mb）',
-    input         text                         not null comment '输入说明',
-    output        text                         not null comment '输出说明',
-    sample_input  text                         not null comment '输入样例',
-    sample_output text                         not null comment '输出样例',
-    hint          text                         not null comment '提示',
+    description   longtext                     not null comment '题面',
+    input         longtext                     not null comment '输入说明',
+    output        longtext                     not null comment '输出说明',
+    sample_input  longtext                     not null comment '输入样例',
+    sample_output longtext                     not null comment '输出样例',
+    hint          longtext                     not null comment '提示',
     create_time   timestamp    default (now()) not null comment '创建时间',
     update_time   timestamp    default (now()) not null comment '更新时间'
 )
@@ -55,7 +56,7 @@ create table if not exists tbl_submission
     length      int unsigned default '0'     not null comment '长度',
     memory      int unsigned default '0'     not null comment '内存',
     time        int unsigned default '0'     not null comment '运行耗时',
-    solution    text                         not null comment '题解'
+    solution    longtext                     not null comment '题解代码'
 )
     comment '提交记录表';
 
@@ -64,8 +65,8 @@ create table if not exists tbl_test_point
     id          int unsigned auto_increment comment '测试点ID'
         primary key,
     problem_id  int unsigned default '0' not null comment '题目ID',
-    test_input  text                     not null comment '测试输入',
-    test_output text                     not null comment '测试输出'
+    test_input  longtext                 not null comment '测试输入',
+    test_output longtext                 not null comment '测试输出'
 )
     comment '测试点表';
 
@@ -77,7 +78,7 @@ create table if not exists tbl_user
     password    varchar(255) default '123456'          not null comment '密码',
     role        int unsigned default '1'               not null comment '角色：0 封禁，1 普通用户，2 管理员，3 超级管理员',
     email       varchar(255)                           not null comment '邮箱',
-    avatar      varchar(255) default ''                not null comment '头像URL',
+    avatar      text                                   not null comment '头像URL',
     create_time timestamp    default CURRENT_TIMESTAMP not null comment '创建时间',
     update_time timestamp    default CURRENT_TIMESTAMP not null comment '更新时间',
     constraint user_pk_2
