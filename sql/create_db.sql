e1c9be7de3ab279bf27a5d11f49ba3519df3dc84
HEX9CF <hex9cf@aliyun.com>
2024-10-24 17:20:18 +0800

    Change data type of time limit to float

--- sql/create_db.sql
+++ sql/create_db.sql
@@ -8,10 +8,14 @@ create table if not exists tbl_judgement
 (
     id            int unsigned auto_increment comment '评测点ID'
         primary key,
-    submission_id int unsigned default '0' not null comment '提交记录ID',
-    status        int unsigned default '0' not null comment '状态',
-    time          int unsigned default '0' not null comment '运行耗时',
-    memory        int unsigned default '0' not null comment '内存'
+    submission_id int unsigned   default '0' not null comment '提交记录ID',
+    test_point_id int unsigned   default '0' not null comment '评测点ID',
+    time          float unsigned default '0' not null comment '运行耗时（s）',
+    memory        int unsigned   default '0' not null comment '内存（kb）',
+    output        longtext                   not null comment '输出结果',
+    error         longtext                   not null comment '报错信息',
+    message       longtext                   not null comment '信息',
+    status        int unsigned   default '0' not null comment '状态'
 )
     comment '单个评测点结果表';
 
@@ -27,20 +31,20 @@ create table if not exists tbl_problem
 (
     id            int unsigned auto_increment comment '题目ID'
         primary key,
-    title         text                         not null comment '标题',
-    source        text                         null comment '题目来源',
-    difficulty    int unsigned default '0'     not null comment '难度： 0 暂无评定，1 普及−，2 普及/提高−，3 普及+/提高，4 提高+/省选− ，5 省选/NOI−，6 NOI/NOI+/CTSC',
-    time_limit    int unsigned default '1'     not null comment '时间限制（秒）',
-    memory_limit  int unsigned default '128'   not null comment '内存限制（mb）',
-    description   longtext                     not null comment '题面',
-    input         longtext                     not null comment '输入说明',
-    output        longtext                     not null comment '输出说明',
-    sample_input  longtext                     not null comment '输入样例',
-    sample_output longtext                     not null comment '输出样例',
-    hint          longtext                     not null comment '提示',
-    status        int unsigned default '1'     not null comment '状态：0 作废，1 公开，2 出题中，3 调试中',
-    create_time   timestamp    default (now()) not null comment '创建时间',
-    update_time   timestamp    default (now()) not null comment '更新时间'
+    title         text                            not null comment '标题',
+    source        text                            null comment '题目来源',
+    difficulty    int unsigned   default '0'      not null comment '难度： 0 暂无评定，1 普及−，2 普及/提高−，3 普及+/提高，4 提高+/省选− ，5 省选/NOI−，6 NOI/NOI+/CTSC',
+    time_limit    float unsigned default '1'      not null comment '时间限制（s）',
+    memory_limit  int unsigned   default '131072' not null comment '内存限制（kb）',
+    description   longtext                        not null comment '题面',
+    input         longtext                        not null comment '输入说明',
+    output        longtext                        not null comment '输出说明',
+    sample_input  longtext                        not null comment '输入样例',
+    sample_output longtext                        not null comment '输出样例',
+    hint          longtext                        not null comment '提示',
+    status        int unsigned   default '1'      not null comment '状态：1 公开，2 出题中，3 调试中，4 作废',
+    create_time   timestamp      default (now())  not null comment '创建时间',
+    update_time   timestamp      default (now())  not null comment '更新时间'
 )
     comment '题目表';
 
@@ -48,16 +52,16 @@ create table if not exists tbl_submission
 (
     id          int unsigned auto_increment comment '提交记录ID'
         primary key,
-    user_id     int unsigned default '0'     not null comment '用户ID',
-    problem_id  int unsigned default '0'     not null comment '题目ID',
-    status      int unsigned default '0'     not null comment '状态：0 未评测',
-    score       int unsigned default '0'     not null comment '分数',
-    submit_time timestamp    default (now()) not null comment '提交时间',
-    language_id int unsigned default '0'     not null comment '语言ID',
-    length      int unsigned default '0'     not null comment '长度',
-    memory      int unsigned default '0'     not null comment '内存',
-    time        int unsigned default '0'     not null comment '运行耗时',
-    source_code longtext                     not null comment '代码'
+    user_id     int unsigned   default '0'     not null comment '用户ID',
+    problem_id  int unsigned   default '0'     not null comment '题目ID',
+    status      int unsigned   default '0'     not null comment '状态：0 未评测',
+    score       int unsigned   default '0'     not null comment '分数',
+    submit_time timestamp      default (now()) not null comment '提交时间',
+    language_id int unsigned   default '0'     not null comment '语言ID',
+    length      int unsigned   default '0'     not null comment '源代码长度',
+    memory      int unsigned   default '0'     not null comment '内存（kb）',
+    time        float unsigned default '0'     not null comment '运行耗时（s）',
+    source_code longtext                       not null comment '源代码'
 )
     comment '提交记录表';
 
