9fc6af9aa12f7b4d0e6aa0158b06e1468a53ef22
HEX9CF <hex9cf@aliyun.com>
2024-10-28 17:38:12 +0800

    Set all columns not null

--- sql/create_db.sql
+++ sql/create_db.sql
@@ -12,10 +12,10 @@ create table if not exists tbl_judgement
     test_point_id  int unsigned   default '0' not null comment '评测点ID',
     time           float unsigned default '0' not null comment '运行耗时（s）',
     memory         int unsigned   default '0' not null comment '内存（kb）',
-    stdout         longtext                   null comment '标准输出',
-    stderr         longtext                   null comment '标准错误输出',
-    compile_output longtext                   null comment '编译输出',
-    message        longtext                   null comment '信息',
+    stdout         longtext                   not null comment '标准输出',
+    stderr         longtext                   not null comment '标准错误输出',
+    compile_output longtext                   not null comment '编译输出',
+    message        longtext                   not null comment '信息',
     status         int unsigned   default '0' not null comment '状态'
 )
     comment '单个评测点结果表';
@@ -33,16 +33,16 @@ create table if not exists tbl_problem
     id            int unsigned auto_increment comment '题目ID'
         primary key,
     title         text                            not null comment '标题',
-    source        text                            null comment '题目来源',
+    source        text                            not null comment '题目来源',
     difficulty    int unsigned   default '0'      not null comment '难度： 0 暂无评定，1 普及−，2 普及/提高−，3 普及+/提高，4 提高+/省选− ，5 省选/NOI−，6 NOI/NOI+/CTSC',
     time_limit    float unsigned default '1'      not null comment '时间限制（s）',
     memory_limit  int unsigned   default '131072' not null comment '内存限制（kb）',
-    description   longtext                        null comment '题面',
-    input         longtext                        null comment '输入说明',
-    output        longtext                        null comment '输出说明',
-    sample_input  longtext                        null comment '输入样例',
-    sample_output longtext                        null comment '输出样例',
-    hint          longtext                        null comment '提示',
+    description   longtext                        not null comment '题面',
+    input         longtext                        not null comment '输入说明',
+    output        longtext                        not null comment '输出说明',
+    sample_input  longtext                        not null comment '输入样例',
+    sample_output longtext                        not null comment '输出样例',
+    hint          longtext                        not null comment '提示',
     status        int unsigned   default '1'      not null comment '状态：1 公开，2 出题中，3 调试中，4 作废',
     create_time   timestamp      default (now())  not null comment '创建时间',
     update_time   timestamp      default (now())  not null comment '更新时间'
@@ -61,7 +61,7 @@ create table if not exists tbl_submission
     length      int unsigned   default '0'     not null comment '源代码长度',
     memory      int unsigned   default '0'     not null comment '内存（kb）',
     time        float unsigned default '0'     not null comment '运行耗时（s）',
-    source_code longtext                       null comment '源代码',
+    source_code longtext                       not null comment '源代码',
     create_time timestamp      default (now()) not null comment '创建时间',
     update_time timestamp      default (now()) not null comment '更新时间'
 )
@@ -86,8 +86,8 @@ create table if not exists tbl_user
     password    varchar(255) default '123456'          not null comment '密码',
     role        int unsigned default '1'               not null comment '角色：0 封禁，1 普通用户，2 管理员，3 超级管理员',
     email       varchar(255)                           not null comment '邮箱',
-    avatar      text                                   null comment '头像URL',
-    signature   text                                   null comment '个性签名',
+    avatar      text                                   not null comment '头像URL',
+    signature   text                                   not null comment '个性签名',
     create_time timestamp    default CURRENT_TIMESTAMP not null comment '创建时间',
     update_time timestamp    default CURRENT_TIMESTAMP not null comment '更新时间',
     constraint user_pk_2
