a445f00da56b604439f67b88c545028012b9a5ee
HEX9CF <hex9cf@aliyun.com>
2024-12-26 17:37:17 +0800

    Add set_blog_content.sql

---
+++ sql/test-data/set_blog_content.sql
@@ -0,0 +1,5 @@
+UPDATE tbl_blog SET content='Lorem ipsum dolor sit amet sed molestie eos enim voluptua eirmod eu elitr rebum et ipsum rebum. Et erat tation et lorem ullamcorper. Doming lorem voluptua amet sea est rebum in. Justo sadipscing eum magna exerci quis nonumy diam erat dolore kasd kasd ipsum.
+
+Vero et et assum diam duis iusto in lorem. Amet odio vero vero sadipscing lorem eos amet zzril sit in aliquyam vulputate erat lorem. Nulla ipsum mazim eirmod diam ipsum diam consetetur at ipsum illum amet ea iriure aliquyam ipsum. Et no sea clita. Aliquip facilisis nonumy gubergren eleifend. Gubergren at dolore.
+
+Iusto et et dolor eirmod. Dolor diam ut praesent facilisi magna dolore molestie invidunt molestie lorem invidunt. Nonumy autem sed hendrerit magna dolores amet veniam dolor option labore augue est. Congue kasd eros voluptua est sit lorem diam feugiat euismod est stet est nonummy takimata nulla ad sanctus eos. Dignissim lorem doming sea justo magna no. Est eum dolor et stet veniam. Consetetur gubergren ut ipsum dignissim facilisis dolore duo dolores ullamcorper dolores kasd ipsum sed duo invidunt amet.' WHERE 1=1;
