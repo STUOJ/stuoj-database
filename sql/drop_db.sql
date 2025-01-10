fa71265882f866ee9e6d15b1d151622a095d6f66
HEX9CF <hex9cf@aliyun.com>
2025-01-10 17:04:30 +0800

    Update drop_db.sql

--- sql/drop_db.sql
+++ sql/drop_db.sql
@@ -1,14 +1,20 @@
 USE stuoj_db;
 
+drop table if exists tbl_blog;
+
+drop table if exists tbl_comment;
+
+drop table if exists tbl_history;
+
 drop table if exists tbl_judgement;
 
 drop table if exists tbl_language;
 
 drop table if exists tbl_problem;
 
-drop table if exists tbl_history;
-
 drop table if exists tbl_problem_tag;
+
+drop table if exists tbl_solution;
 
 drop table if exists tbl_submission;
 
