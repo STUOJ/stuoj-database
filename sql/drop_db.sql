713166001fc10858ff1d5ea30a70cc9433fa7ac8
HEX9CF <hex9cf@aliyun.com>
2025-01-10 17:56:36 +0800

    Use DROP CASCADE

--- sql/drop_db.sql
+++ sql/drop_db.sql
@@ -1,26 +1,26 @@
 USE stuoj_db;
 
-drop table if exists tbl_blog;
+drop table if exists tbl_comment cascade;
 
-drop table if exists tbl_comment;
+drop table if exists tbl_blog cascade;
 
-drop table if exists tbl_history;
+drop table if exists tbl_history cascade;
 
-drop table if exists tbl_judgement;
+drop table if exists tbl_judgement cascade;
 
-drop table if exists tbl_language;
+drop table if exists tbl_problem_tag cascade;
 
-drop table if exists tbl_problem;
+drop table if exists tbl_solution cascade;
 
-drop table if exists tbl_problem_tag;
+drop table if exists tbl_submission cascade;
 
-drop table if exists tbl_solution;
+drop table if exists tbl_language cascade;
 
-drop table if exists tbl_submission;
+drop table if exists tbl_tag cascade;
 
-drop table if exists tbl_tag;
+drop table if exists tbl_testcase cascade;
 
-drop table if exists tbl_testcase;
+drop table if exists tbl_problem cascade;
 
-drop table if exists tbl_user;
+drop table if exists tbl_user cascade;
 
